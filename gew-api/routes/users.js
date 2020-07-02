1d2f7184f038efaf4ec79da1ef8c60c507c60e23
Ricardo Canela <ricardocanela.ns@gmail.com>
2020-07-02 17:56:13 +0100

    atenticação

--- gew-api/routes/users.js
+++ gew-api/routes/users.js
@@ -2,43 +2,30 @@ var express = require('express');
 var router = express.Router();
 var Users = require('../controllers/users');
 var verifyToken = require("../config/auth").verifyToken;
-var jwt = require('jsonwebtoken');
+var authLogin = require("../config/auth").authLogin;
 
 router.post('/register', (req, res) => {
     // Mock user
     const user = req.body
     Users.newUser(user)
       .then(user => res.json(user))
-      .catch(e => res.status(500).send(`Erro no registro do utilizador ${user.body.name}: ${e}`))
+      .catch(e => res.status(500).send(`Erro no registro do utilizador ${user.name}: ${e}`))
   });
   
 router.post('/authenticate', (req, res) => {
     Users.getUser(req.body.username)
-        .then(user => {
+        .then(async user => {
             if(user && user.password === req.body.password) {
-                jwt.sign({ user: req.body }, 'secretkey', { expiresIn: '3000s' }, (err, token) => {
-                    res.json({
-                        token
-                    });
+                token = await authLogin(req.body)
+                console.log('2--->',token)
+                res.json({
+                    token
                 });
             } else {
                 res.jsonp({ msg: 'Credenciais inválidas' })  
             }
         })
         .catch(err => res.jsonp(err));
-});
-
-router.get('/authenticated', verifyToken, (req, res) => {  
-    jwt.verify(req.token, 'secretkey', (err, authData) => {
-        if(err) {
-            res.sendStatus(403);
-        } else {
-            res.json({
-                message: 'Utilizador autenticado!',
-                authData
-            });
-        }
-    });
 });
 
 router.get('/favorites/:user', verifyToken, (req, res) => {
